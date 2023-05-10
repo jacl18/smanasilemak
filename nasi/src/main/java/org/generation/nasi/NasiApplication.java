fbd277bc402c0790c3f975ed3cb5d8f707b40af8
jasgoh92 <127270592+jasgoh92@users.noreply.github.com>
2023-05-10 16:00:35 +0800

    Add files via upload
    upload

---
+++ nasi/src/main/java/org/generation/nasi/NasiApplication.java
@@ -0,0 +1,13 @@
+package org.generation.nasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasiApplication.class, args);
+	}
+
+}
